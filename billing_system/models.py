6f30393655ed6607fef7569e0679116a950d97a4
Shahil Malik <shahil@victopialabs.com>
2024-12-29 19:39:58 +0530

    Models, views, serializers and everything else

---
+++ billing_system/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+
+# Address Model
+class Address(models.Model):
+    line1 = models.CharField(max_length=255)
+    line2 = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=6)
+
+# Business Domain Model
+class BusinessDomain(models.Model):
+    name = models.CharField(max_length=255)
+
+# CustomUser Model
+class CustomUser(models.Model):
+    name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True, blank=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
+
+# Client Model
+class Client(models.Model):
+    business_name = models.CharField(max_length=255)
+    gst_number = models.CharField(max_length=15)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
+    total_spend = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    business_domain = models.ForeignKey(BusinessDomain, on_delete=models.SET_NULL, blank=True, null=True)
+    note = models.TextField(null=True, blank=True)
+
+# Designation Model
+class Designation(models.Model):
+    name = models.CharField(max_length=255)
+
+# BankDetails Model
+class BankDetails(models.Model):
+    account_number = models.CharField(max_length=20)
+    bank_name = models.CharField(max_length=255)
+    ifsc_code = models.CharField(max_length=11)
+
+
+# Employee Model
+class Employee(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    designation = models.ForeignKey(Designation, on_delete=models.SET_NULL, null=True)
+    joining_date = models.DateField()
+    is_authorizer = models.BooleanField(default=False)
+    signature = models.ImageField(upload_to='signatures/', null=True, blank=True)
+    system_number = models.CharField(max_length=255)
+    bank_details = models.OneToOneField(BankDetails, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name, self.designation, self.signature
+
+# Service Model
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+# PaymentMode Model
+class PaymentMode(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+# PaymentTerms Model
+class PaymentTerm(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+# Invoice Model
+class Invoice(models.Model):
+    number = models.CharField(max_length=255)
+    place = models.CharField(max_length=255)
+    date = models.DateField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    payment_mode = models.ForeignKey(PaymentMode, on_delete=models.SET_NULL, null=True)
+    payment_terms = models.ForeignKey(PaymentTerm, on_delete=models.SET_NULL, null=True)
+    estimated_completion_date = models.DateField()
+    authorizer = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    STATUS_CHOICES = [
+        ("unsettled","Unsettled"),
+        ("settled","settled"),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="unsettled")
---
+++ billing_system/views.py
@@ -0,0 +1,61 @@
+from rest_framework import viewsets
+from .models import Client, Invoice, Service, Employee, PaymentMode, PaymentTerm, Address, BusinessDomain
+from .serializers import ClientSerializer, InvoiceSerializer, ServiceSerializer, EmployeeSerializer, PaymentModeSerializer, AddressSerializer, BusinessDomainSerializer,DropdownDataSerializer, PaymentTermSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class PaymentModeViewSet(viewsets.ModelViewSet):
+    queryset = PaymentMode.objects.all()
+    serializer_class = PaymentModeSerializer
+
+
+class PaymentTermViewSet(viewsets.ModelViewSet):
+    queryset = PaymentTerm.objects.all()
+    serializer_class = PaymentTermSerializer
+
+
+
+class InvoiceDropdownDataView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        latest_invoice = Invoice.objects.order_by('-id').first()
+        if not latest_invoice:
+            latest_invoice = 0
+        payment_terms = PaymentTerm.objects.all().values()  
+        payment_modes = PaymentMode.objects.all().values()  
+        clients = Client.objects.all().values('id', 'business_name') 
+        services = Service.objects.filter(is_available=True).values('id', 'name','cost')
+
+        data = {
+            'invoice_number': str(latest_invoice + 1).zfill(3),
+            'payment_terms': list(payment_terms),
+            'payment_modes': list(payment_modes),
+            'clients': list(clients),
+            'services': list(services),
+
+        }
+
+        return Response(data)

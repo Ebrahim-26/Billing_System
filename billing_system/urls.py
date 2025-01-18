35a312afa4c427c92b882c2847b887c5170d4cbe
Shahil Malik <shahilabdul001@gmail.com>
2025-01-18 23:06:51 +0530

    Update urls.py

--- billing_system/urls.py
+++ billing_system/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("management-portal/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
 ]

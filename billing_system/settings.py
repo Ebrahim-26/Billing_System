26b2112f10bae4c57cc881dd4bc3e33354756b10
shahil <shahilabdul001@gmail.com>
2025-01-19 20:02:04 +0530

    settings revert

--- billing_system/settings.py
+++ billing_system/settings.py
@@ -148,43 +148,7 @@ CORS_ALLOW_HEADERS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-print("In prod settings")
-DEBUG = False
-ALLOWED_HOSTS = ['www.tarvizdigimart.com','13.127.183.30']
-
-CORS_ALLOWED_ORIGINS = [
-    'https://www.tarvizdigimart.com',
-    "http://13.127.183.30",
-    "https://13.127.183.30",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://www.tarvizdigimart.com',
-    "http://13.127.183.30",
-    "https://13.127.183.30",
-]
-
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
-
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_HTTPONLY = True
-
-DEBUG = True
-
-# Static files
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Add Next.js static files directory
-NEXT_STATIC_DIR = os.path.join(BASE_DIR, 'billing_portal_fe', '.next', 'static')
-STATICFILES_DIRS = [
-    NEXT_STATIC_DIR,  # Add Next.js static files to Django
-]
-
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-~
-~
-  
+if DJANGO_ENV == 'production':
+    from .production_settings import *  
+else:
+    from .development_settings import *  ``
